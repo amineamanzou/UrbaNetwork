4862c83f215fc0e306fc3ca72c82e8d76b51a264
amineamanzou <amineamanzou@gmail.com>
2013-02-13 21:03:20 +0100

    Adding and configuring SonataMediaBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,6 +36,8 @@ class AppKernel extends Kernel
             new Urbanet\UserBundle\UrbanetUserBundle(),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
             
+            new Sonata\MediaBundle\SonataMediaBundle(),
+            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
