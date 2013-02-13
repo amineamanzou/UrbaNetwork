eb0317083d3c36667ee09a060bf9a68fea6b79eb
amineamanzou <amineamanzou@gmail.com>
2013-02-13 20:05:54 +0100

    Updating User Entity
    To use the generated SonataUserBundle

--- src/Urbanet/UserBundle/Entity/User.php
+++ src/Urbanet/UserBundle/Entity/User.php
@@ -1,7 +1,7 @@
 <?php
 namespace Urbanet\UserBundle\Entity;
  
-use FOS\UserBundle\Entity\User as BaseUser;
+use Sonata\UserBundle\Entity\BaseUser as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
  
 /**
