d93d3a636aad2affddd6c793e51fd7fb41d1c111
Amine Amanzou <amineamanzou@gmail.com>
2013-02-26 16:15:43 +0100

    Adding Cv view

--- src/Urbanet/CvBundle/Controller/DefaultController.php
+++ src/Urbanet/CvBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('UrbanetCvBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('UrbanetCvBundle:Default:index.html.twig');
+    }
+    
+    public function cvAction()
+    {
+        return $this->render('UrbanetCvBundle:Default:cv.html.twig');
     }
 }
