544577e54c7a55e6b9da988e007bad55211dc66c
Raph210 <semlali77210@gmail.com>
2013-03-10 17:44:18 +0100

    Adding Form (Bundle CV )
    - Ajout d'informations  (Photo / Titre CV)
    - Modification informations
    - Affichage de "mon CV"
    
    nb : La première fois que l'utilisateur arrive sur la partie CV il est
    automatiquement redirigé vers la page Ajout Informations

--- src/Urbanet/CvBundle/Controller/DefaultController.php
+++ src/Urbanet/CvBundle/Controller/DefaultController.php
@@ -3,23 +3,83 @@
 namespace Urbanet\CvBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Urbanet\CvBundle\Entity\CVArt;
+use Urbanet\CvBundle\Form\CVArtType;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function voirAction($id)
     {
-
-
-
-
-
-
-
-        return $this->render('UrbanetCvBundle:Default:index.html.twig');
+        if(!$id)
+            return $this->redirect($this->generateUrl("urbanet_cv_ajouter"));
+            
+        $em = $this->getDoctrine()->getEntityManager();
+        $CVArt = $em->getRepository("UrbanetCvBundle:CVArt")->findOneById($id);
+   	    return $this->render('UrbanetCvBundle:Default:voir.html.twig', array(
+   	    	'CVArt' => $CVArt,
+   	    	));
+   	    
     }
-    
-    public function editerAction()
+
+    public function ajouterAction()
     {
-        return $this->render('UrbanetCvBundle:Default:editer.html.twig');
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $CVArt = new CVArt();
+        $form = $this->createForm(new CVArtType(), $CVArt);
+        $request = $this->getRequest();
+        
+        if($request->isMethod('POST'))
+        {
+            $form->bindRequest($request);
+            
+            if ($form->isValid()){
+
+                $CVArt = $form->getData();
+                $em->persist($CVArt);
+                $em->flush();
+            
+                return $this->redirect($this->generateUrl("urbanet_cv_voir", array(
+                            'id' => $CVArt->getId(),	
+                            )));
+            }
+            
+        }
+        
+        
+        return $this->render('UrbanetCvBundle:Default:ajouter.html.twig', array(
+            'id'   => $CVArt->getId(),
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function editerAction(CVArt $CVArt)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $form = $this->createForm(new CVArtType(), $CVArt);
+        $request = $this->getRequest();
+        
+        if($request->isMethod('POST'))
+        {
+            $form->bindRequest($request);
+            
+            if ($form->isValid()){
+
+                $CVArt = $form->getData();
+                $em->persist($CVArt);
+                $em->flush();
+            
+                return $this->redirect(
+                		$this->generateUrl("urbanet_cv_voir", array(
+                            'id' => $CVArt->getId(),    
+                            )));
+            }
+        }
+            
+        return $this->render('UrbanetCvBundle:Default:editer.html.twig', array(
+            'id'   => $CVArt->getId(),
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/Urbanet/CvBundle/Form/CVArtType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Urbanet\CvBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CVArtType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Titre')
+            ->add('Photo')
+            /*
+            ->add('Spectacle')
+            ->add('Formation')
+            */
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Urbanet\CvBundle\Entity\CVArt'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'urbanet_cvbundle_cvarttype';
+    }
+}
