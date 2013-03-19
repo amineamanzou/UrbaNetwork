1ffcc9a06353c3ce89dde9b3c3a44a2a7a4b7bac
Amine Amanzou <amineamanzou@gmail.com>
2013-03-19 15:43:05 +0100

    Adding Facebook parameters

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\FacebookBundle\FOSFacebookBundle(),
             
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Sonata\CacheBundle\SonataCacheBundle(),
