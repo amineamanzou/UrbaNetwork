f67d15fee1e240a6fdef1c17e347bca4d6168d62
Amine Amanzou <amineamanzou@gmail.com>
2013-03-19 15:43:48 +0100

    Trying to extends FOSUserBundle

--- src/Urbanet/UserBundle/UrbanetUserBundle.php
+++ src/Urbanet/UserBundle/UrbanetUserBundle.php
@@ -6,4 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UrbanetUserBundle extends Bundle
 {
+    public function getParent() {
+        return 'ApplicationSonataUserBundle';
+    }
 }
