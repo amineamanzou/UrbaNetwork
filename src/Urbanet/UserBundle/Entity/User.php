cd93265bfe1778c629d7ec0c6f2e9e66dfbc377c
Amine Amanzou <amineamanzou@gmail.com>
2013-03-29 21:26:16 +0100

    Trying to get Facebook connection functionnal : Failure

--- src/Urbanet/UserBundle/Entity/User.php
+++ src/Urbanet/UserBundle/Entity/User.php
@@ -1,7 +1,8 @@
 <?php
 namespace Urbanet\UserBundle\Entity;
  
-use Sonata\UserBundle\Entity\BaseUser as BaseUser;
+//use Sonata\UserBundle\Entity\BaseUser as BaseUser;
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
  
 /**
@@ -16,11 +17,125 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
- 
+    
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $facebookID;
+    
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $firstname;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $lastname;
+    
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+        // your own logic        
     }
+    
+    public function serialize()
+    {
+        return serialize(array($this->facebookID, parent::serialize()));
+    }
+
+    public function unserialize($data)
+    {
+        list($this->facebookID, $parentData) = unserialize($data);
+        parent::unserialize($parentData);
+    }
+    
+    /**
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname($firstname=null)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname($lastname=null)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * Get the full name of the user (first + last name)
+     * @return string
+     */
+    public function getFullName()
+    {
+        return $this->getFirstName() . ' ' . $this->getLastname();
+    }
+
+    /**
+     * @param string $facebookID
+     * @return void
+     */
+    public function setFacebookID($facebookID=null)
+    {
+        $this->facebookID = $facebookID;        
+        if ($this->username=="")    //si on n'a pas de username
+        {
+            //on met le facebook id a la place
+            $this->setUsername($facebookID);
+        }
+        $this->salt = '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getFacebookID()
+    {
+        return $this->facebookID;
+    }
+
+    /**
+     * @param Array
+     */
+    public function setFBData($fbdata)
+    {        
+        if (isset($fbdata['id'])) {
+            $this->setFacebookID($fbdata['id']);
+            $this->addRole('ROLE_FACEBOOK');
+        }
+        if (isset($fbdata['first_name'])) {
+            $this->setFirstname($fbdata['first_name']);
+        }
+        if (isset($fbdata['last_name'])) {
+            $this->setLastname($fbdata['last_name']);
+        }
+        if (isset($fbdata['email'])) {
+            $this->setEmail($fbdata['email']);
+        }
+    }
+    
 }
 ?>
